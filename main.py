92ec966aacc74d60e46c38c98282117c3c788edc
msattova <msattova@gmail.com>
2022-02-23 18:57:48 +0900

    delete func.py and add app.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from bs4 import BeautifulSoup
-import os
 import modules.arg as arg
-import modules.func as func
+import modules.app as app
 
 cmd = arg.CmdArg()
 
@@ -9,56 +7,9 @@ path: str = cmd.args.filename
 only: str = cmd.args.only
 exclude: list[str] = cmd.args.exclude
 notab: bool = cmd.args.notab
+leave: bool = cmd.args.leave
+output_name = cmd.args.output
+output_dir = './output/'
 
-with open(path, encoding='utf-8') as f:
-  str = f.read()
-
-basename = os.path.basename(path)
-filename = os.path.splitext(basename)[0]
-
-outputdir = "./output/"
-
-if cmd.args.leave:
-  output_name = filename + '.md'
-else:
-  output_name = cmd.args.output
-
-soup = BeautifulSoup(str, 'html.parser')
-messages = soup.find_all('p')
-meslist = []
-get_inner = slice(1, -1)
-for msg in messages:
-  inlist = []
-  for span in msg.find_all('span'):
-    str = span.string
-    if str is not None:
-      str = str.strip()
-    else:
-      str = ''
-    if '#' in str:
-      str = str.replace('#', '\\#')
-    inlist.append(str)
-  inlist[0] = inlist[0][get_inner]
-  meslist.append(inlist)
-
-out = f"# {filename}\n\n"
-
-meslist = list(filter(lambda m: m[2] != '', meslist))  # 発言が空ならスキップ
-
-for m in meslist:
-  tab = m[0]  # タブ名
-  name = m[1]  # 名前
-  mention = m[2]  # 発言
-  if only is not None and only not in tab:
-    continue
-  if exclude is not None and tab in exclude:
-    continue
-  if name == '':  # 名前が空だったときの処理
-    name = ' '
-  out += func.makeoutstr(tab, name, mention, notab)
-
-if not os.path.exists(outputdir):
-  os.mkdir(outputdir)
-
-with open(outputdir+output_name, mode='w', encoding='utf-8') as f:
-  f.write(out)
+apcv = app.Convert(path, output_name, output_dir, exclude, only, leave, notab)
+apcv.run()
---
+++ modules/app.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+import os
+
+class Convert():
+
+  def __init__(self,
+              path: str, output_name:str,
+              output_dir: str,
+              excludes: list[str],
+              is_only: bool, is_leave: bool, is_notab: bool):
+    with open(path, encoding='utf-8') as f:
+      str = f.read()
+
+    basename = os.path.basename(path)
+    filename = os.path.splitext(basename)[0]
+
+    self.meslist = list()
+    self.output_dir = output_dir
+    self.excludes = excludes
+    self.is_only = is_only
+    self.is_leave = is_leave
+    self.is_notab = is_notab
+    self.output_name = output_name
+    self.output_name = filename + '.md' if self.is_leave else self.output_name
+
+    self.out = f"# {os.path.splitext(self.output_name)[0]}\n\n"
+    self.soup = BeautifulSoup(str, 'html.parser')
+
+  def __search_and_get(self):
+    messages = self.soup.find_all('p')
+    get_inner = slice(1, -1)
+    for msg in messages:
+      inlist = []
+      for span in msg.find_all('span'):
+        str = span.string
+        if str is not None:
+          str = str.strip()
+        else:
+          str = ''
+        if '#' in str:
+          str = str.replace('#', '\\#')
+        inlist.append(str)
+      inlist[0] = inlist[0][get_inner] # タブ名の[]の内側の文字列を取得
+      self.meslist.append(inlist)
+
+  def __make_outstr(self):
+    self.meslist = list(filter(lambda m: m[2] != '', self.meslist))  # 発言が空ならスキップ
+    for m in self.meslist:
+      tab = m[0]  # タブ名
+      name = m[1]  # 名前
+      mention = m[2]  # 発言
+      if self.is_only is not None and self.is_only not in tab:
+        continue
+      if self.excludes is not None and tab in self.excludes:
+        continue
+      if name == '':  # 名前が空だったときの処理
+        name = ' '
+      self.out += self.__makestr(tab, name, mention, self.is_notab)
+
+  def __out_file(self):
+    if not os.path.exists(self.output_dir):
+      os.mkdir(self.output_dir)
+
+    with open(self.output_dir+self.output_name, mode='w', encoding='utf-8') as f:
+      f.write(self.out)
+
+  def __makestr(self, tab: str, name: str,
+                  mention: str, is_notab=False,
+                  blacket=('【', '】'), em='**') -> str:
+    if is_notab:
+      return f"{em}{name}{em} : {mention}\n\n"
+    else:
+      return f"{blacket[0]}{tab}{blacket[1]} {em}{name}{em} : {mention}\n\n"
+
+  def run(self):
+    self.__search_and_get()
+    self.__make_outstr()
+    self.__out_file()
