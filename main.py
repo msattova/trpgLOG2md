a35faeca2116f17f8559117132a26b26036ace2e
msattova <msattova@gmail.com>
2022-02-27 18:43:07 +0900

    コードを整形・変更 #4

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import modules.arg as arg
 import modules.app as app
 
-cmd = arg.CmdArg()
+if __name__ == '__main__':
 
-path: str = cmd.args.filename
-only: str = cmd.args.only
-exclude: list[str] = cmd.args.exclude
-notab: bool = cmd.args.notab
-leave: bool = cmd.args.leave
-output_name = cmd.args.output
-output_dir = './output/'
+    cmd = arg.CmdArg()
 
-app.main(path, output_name, output_dir, exclude, only, leave, notab)
+    path: str = cmd.args.filename
+    only: str = cmd.args.only
+    exclude: list[str] = cmd.args.exclude
+    notab: bool = cmd.args.notab
+    leave: bool = cmd.args.leave
+    output_name = cmd.args.output
+    output_dir = './output/'
+
+    app.main(path, output_name, output_dir, exclude, only, leave, notab)
